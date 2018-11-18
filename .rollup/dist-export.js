1c4a07f71a3296380d816a4a9b38abdaa8a49ac6
n24651 <cristobal.dabed@nrk.no>
2018-11-18 05:34:56 +0100

    * Add guard cheks for implementations and polyfills

--- .rollup/dist-export.js
+++ .rollup/dist-export.js
@@ -1,2 +1,14 @@
-/* global window */
+/* Guard checks for Promise, Fetch and AbortController implementations */
+if (!Object.prototype.toString.call(typeof Promise !== 'undefined' ? Promise : 0)) {
+  throw new Error('No Promise available implementation please ensure a polyfill available')
+}
+
+if (!Object.prototype.toString.call(typeof fetch !== 'undefined' ? fetch : 0)) {
+  throw new Error('No fetch available implementation please ensure a polyfill available')
+}
+
+if (!Object.prototype.toString.call(typeof AbortController !== 'undefined' ? AbortController : 0)) {
+  throw new Error('No AbortController implementation available please ensure a polyfill available')
+}
+
 export { default } from '../src/fetch-with-retry.js'

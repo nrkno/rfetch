555a7a194b11cfaa1c8816779f061ddf7f0be771
n24651 <cristobal.dabed@nrk.no>
2018-11-19 22:18:37 +0100

    Docs typo _Rollup_ not _Roll up_

--- src/impl/abort-controller-impl.js
+++ src/impl/abort-controller-impl.js
@@ -1,7 +1,7 @@
 import AbortController from 'abort-controller'
 
 /*
-  Roll up target is web, use window.AbortController,
+  Rollup target is web, use window.AbortController,
   otherwise use AbortController from the `abort-controller` library
 */
 const AbortControllerImpl = process.env.rollupTargetWeb
--- src/impl/fetch-impl.js
+++ src/impl/fetch-impl.js
@@ -4,7 +4,7 @@ import nodeFetch from 'node-fetch'
 const definitions = {}
 
 /*
-  Roll up target is web, use window.fetch,
+  Rollup target is web, use window.fetch,
   otherwise use fetch from the `node-fetch` library
 */
 definitions.fetch = process.env.rollupTargetWeb
